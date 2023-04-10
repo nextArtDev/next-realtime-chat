32423aba59ceb1af137a7d4b3b698899076f8610
Saeid Mihmanparast <nextartisticdevelopments@gmail.com>
2023-04-10 18:28:56 +0430

    login UI

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/ui/Button'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return <Button>page</Button>
+}
+
+export default page
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import Button from '@/components/ui/Button'
+
+export default function Home() {
+  return <Button variant={'ghost'}>Hello</Button>
+}
